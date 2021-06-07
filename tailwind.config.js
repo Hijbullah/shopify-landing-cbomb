e4a038e993e8cdd04fa11b83c8af9a329eebc7d9
Hijbullah Amin <hijbullaah@gmail.com>
2021-06-08 00:42:18 +0600

    project initialize

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './layout/*.liquid',
+    './templates/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
